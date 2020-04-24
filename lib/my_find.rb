95247b08a61088e7297c20fbed25fa74320dc5da
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-24 13:36:33 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,9 +5,7 @@ def my_find(collection)
   i = 0
   
   while i < collection.length do
-    if yield(collection[i])
-      return collection[i]
-    end
+    if yield(collection[i]) return collection[i]
     i += 1
   end
 end
